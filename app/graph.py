c65f4a15e7af2ff8a6c6d6236d7417c0b3d3db78
leo-devoteam <leo.pacheco@devoteam.com>
2025-02-06 13:46:26 +0100

    relocalized graph and memory instance into graph.py

--- app/graph.py
+++ app/graph.py
@@ -5,9 +5,14 @@ from state import State
 from query_output import write_query
 from query_executor import execute_query
 from answer import generate_answer
+from memory import memory
 
 graph_builder = StateGraph(State).add_sequence(
     [write_query, execute_query, generate_answer]
 )
 graph_builder.add_edge(START, "write_query")
-# graph = graph_builder.compile()
+graph = graph_builder.compile(checkpointer=memory, interrupt_before=["execute_query"])
+
+# Now that we're using persistence, we need to specify a thread ID
+# so that we can continue the run after review.
+config = {"configurable": {"thread_id": "1"}}
--- app/memory.py
+++ app/memory.py
@@ -1,11 +1,3 @@
 from langgraph.checkpoint.memory import MemorySaver
 
-from graph import graph_builder
-
-
 memory = MemorySaver()
-graph = graph_builder.compile(checkpointer=memory, interrupt_before=["execute_query"])
-
-# Now that we're using persistence, we need to specify a thread ID
-# so that we can continue the run after review.
-config = {"configurable": {"thread_id": "1"}}
